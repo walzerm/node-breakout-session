1a6da703966cf2f731b39658e0e7c55566ee4544
Lissa Walzer <lissawalzer@gmail.com>
2015-12-15 15:00:37 -0800

    Got the routes file working for the pixel maker

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ var express = require('express');
 var app = express();
 
 // Set the view engine && public folder
-app.set('view engine', 'html');
-app.set('views', __dirname);
+app.set('view enigne', 'ejs');
+app.set('views', __dirname + '/pixelArt/views');
 app.use(express.static(__dirname + '/assets'));
 
-// Register root route
-app.get('/pixelArt', function(req, res) {
-  res.sendFile(__dirname + '/pixelArt/views/index.html');
-});
+var pixelRoutes = require('./pixelArt/controllers/routes');
+app.use('/pixelArt', pixelRoutes);
 
 var server = app.listen(8000, function () {
   var port = server.address().port;
